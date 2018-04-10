2fcfc9e5c39588cbd8caac1bc0126a5c45005013
zly <zhonglingye@jkinfo.com.cn>
2018-04-10 17:20:01 +0800

    chore: 移除修改为typescript部分
    该部分转移到refactor-to-typescript分支上

--- src/functional/matchString.ts
+++
@@ -1,6 +0,0 @@
-
-export default (strings:string, reference:string) : boolean=>{
-    let patten = `^(.*${reference}.*)`;
-    let reg = new RegExp(patten);
-    return reg.test(strings);
-}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import functional  from "./functional";
-import responseAccordingAccept from "./expressmidware/requestAccept";
-
-
-export { functional, responseAccordingAccept};
-
-
